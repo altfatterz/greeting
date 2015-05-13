ac494c9eebd59875fc24c7d973b7b827dd9d65c8
Zoltan Altfatter <altfatterz@gmail.com>
2015-05-14 00:12:22 +0200

    more debug

--- src/main/java/com/elerna/ForceHerokuSsl.java
+++ src/main/java/com/elerna/ForceHerokuSsl.java
@@ -18,7 +18,11 @@ public class ForceHerokuSsl extends HandlerInterceptorAdapter {
 
             System.out.println("URL:" + request.getRequestURL());
             System.out.println("URI:" + request.getRequestURI());
-            System.out.println("PathInfo: " + request.getPathInfo());
+            System.out.println("PathInfo:" + request.getPathInfo());
+            System.out.println("Path translated:" + request.getPathTranslated());
+            System.out.println("Context path:" + request.getContextPath());
+            System.out.println("Servlet path:" + request.getServletPath());
+            System.out.println("Query string:" + request.getQueryString());
 
             String location = "https://" + request.getRequestURL().substring("http://".length());
             System.out.println("Location:" + location);
